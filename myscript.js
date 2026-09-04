72461779ce9fbc6df6a8efc5dd71e632a58bf46b
octocat30-prog <octocat30@gmail.com>
2026-09-04 13:50:07 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -2,8 +2,14 @@ $(document).ready(function() {
 	$("#imagetoggler").click(function(){
 		$("img").toggle();
 	});
+
 	$("h1").click(function() {
 		$("p").addClass("bright");
 		$("img").addClass("border");
+	});
+
+	// Click the 1st paragraph to toggle visibility of the 2nd and 3rd paragraphs
+	$("p:first").click(function() {
+		$("p:nth-of-type(2), p:nth-of-type(3)").toggle();
 	});
 });
